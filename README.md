a95afa88f258220a6e305a4d656bc869167f9c91
Ali Yara <ali.yara.fr@gmail.com>
2024-11-29 11:14:45 +0100

    add readme file

---
+++ README.md
@@ -0,0 +1,75 @@
+# Gestion d'Équipe Ultime
+
+Une application web pour gérer des équipes de football, conçue pour simplifier l'organisation des joueurs, changer les formations et conserver les données grâce au stockage local.
+
+## Table des Matières
+
+- [Fonctionnalités](#fonctionnalités)
+- [Technologies Utilisées](#technologies-utilisées)
+- [Installation](#installation)
+- [Utilisation](#utilisation)
+- [Captures d'Écran](#captures-décran)
+- [Contributions](#contributions)
+- [Licence](#licence)
+- [Remerciements](#remerciements)
+
+## Fonctionnalités
+
+- **Glisser-Déposer** : Organisez facilement les joueurs en les glissant dans les positions sur le terrain.
+- **Changement de Formation** : Changez rapidement entre différentes formations (par exemple, 4-3-3, 4-4-2, 3-4-3) à l'aide d'un menu déroulant convivial.
+- **Gestion des Substitutions Intelligente** : Déplacez automatiquement les joueurs vers la liste des remplaçants si leur position désignée est déjà occupée, évitant ainsi les conflits.
+- **Persistance des Données** : Utilisez le stockage local pour enregistrer les données de l'équipe, garantissant que les informations restent intactes même après un rafraîchissement de la page.
+- **Interface Conviviale** : Un design propre et intuitif qui améliore l'expérience utilisateur tant pour les novices que pour les utilisateurs expérimentés.
+
+## Technologies Utilisées
+
+- **HTML** : Pour structurer l'application web.
+- **CSS** : Pour le style et la mise en page afin d'améliorer l'attrait visuel.
+- **JavaScript** : Pour l'interactivité et la gestion de la fonctionnalité de glisser-déposer, ainsi que la manipulation des données.
+
+## Installation
+
+Pour configurer le projet localement, suivez ces étapes :
+
+1. **Clonez le dépôt** :
+   ```bash
+   git clone https://github.com/aliyara290/ultimate-team.git
+   ```
+2. **Naviguez dans le répertoire du projet** :
+   ```bash
+   cd ultimate-team
+   ```
+3. **Ouvrez `index.html` dans votre navigateur web** pour voir l'application.
+
+## Utilisation
+
+1. **Ajoutez des Joueurs** : Entrez les détails des joueurs dans les champs de saisie fournis et ajoutez-les à l'équipe.
+2. **Organisez les Joueurs** : Glissez et déposez les joueurs dans leurs positions respectives sur le terrain.
+3. **Changez la Formation** : Utilisez le menu déroulant pour changer les formations selon vos besoins.
+4. **Gestion du Rafraîchissement** : Testez l'application en rafraîchissant la page pour voir que vos modifications sont préservées grâce au stockage local.
+
+## Captures d'Écran
+
+![Capture d'Écran de Gestion d'Équipe](./public/images/bg/project-caps.png)  
+*Exemple de l'interface de gestion d'équipe.*
+
+## Contributions
+
+Les contributions sont les bienvenues ! Si vous souhaitez contribuer, veuillez suivre ces étapes :
+
+1. Forkez le dépôt.
+2. Créez une nouvelle branche (`git checkout -b feature/VotreFonctionnalité`).
+3. Engagez vos modifications (`git commit -m 'Ajoutez une fonctionnalité'`).
+4. Poussez vers la branche (`git push origin feature/VotreFonctionnalité`).
+5. Ouvrez une demande de tirage.
+
+Veuillez vous assurer que votre code respecte les directives de style du projet et qu'il est soigneusement testé.
+
+## Licence
+
+Ce projet est sous licence MIT. Consultez le fichier [LICENSE](LICENSE) pour plus de détails.
+
+## Remerciements
+
+- Merci à la communauté open-source pour son soutien continu et ses ressources.
+- Un grand merci à tous ceux qui ont contribué au projet, fourni des commentaires ou inspiré son développement.
--- src/js/main.js
+++ src/js/main.js
@@ -14,14 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const savedFormation = localStorage.getItem("formation");
   if (savedFormation) {
-    formation.value = savedFormation; 
-    updateFormationLayout(savedFormation); 
+    formation.value = savedFormation;
+    updateFormationLayout(savedFormation);
   }
 });
 
 function reportActivity() {
   console.log('The sort order has changed');
 }
+
+// handle player card setting toggle
 
 const formation = document.querySelector("#formation");
 const squadList = document.querySelector(".squad__list-content");
@@ -30,13 +32,13 @@ const squadList = document.querySelector(".squad__list-content");
 formation.addEventListener("change", (e) => {
   const target = e.target.value;
 
-  localStorage.setItem("formation", target); 
-  
+  localStorage.setItem("formation", target);
+
   updateFormationLayout(target);
 });
 
 function updateFormationLayout(target) {
-  squadList.style.gridTemplateColumns = "repeat(8, 1fr)"; 
+  squadList.style.gridTemplateColumns = "repeat(8, 1fr)";
 
   switch (target) {
     case "433":
@@ -71,7 +73,6 @@ function updateFormationLayout(target) {
       break;
   }
 }
-
 
 let players = [];
 
@@ -122,6 +123,7 @@ playerForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const player = {
+    id: Date.now().toString(),
     name: document.getElementById("o-player-name").value,
     nationalite: document.getElementById("o-player-nationalite").value,
     club: document.getElementById("o-player-club").value,
@@ -135,9 +137,7 @@ playerForm.addEventListener("submit", (e) => {
     rating: document.getElementById("o-player-rating").value,
   };
 
-  const existingPlayerIndex = players.findIndex(
-    (p) => p.name === player.name
-  );
+  const existingPlayerIndex = players.findIndex((p) => p.name === player.name);
 
   if (existingPlayerIndex !== -1) {
     players[existingPlayerIndex] = player;
@@ -149,6 +149,9 @@ playerForm.addEventListener("submit", (e) => {
   showPlayerCard();
   playerForm.reset();
 });
+
+
+
 
 const showPlayerCard = () => {
   const substitute = document.querySelector("#substitutes__list");
@@ -173,6 +176,126 @@ const showPlayerCard = () => {
                   <div class="player__stats">
                     <div class="player__picture">
                       <img src="./images/players-pics/benzema.webp" alt="" />
+                    </div>
+                     <div class="player__setting">
+                      <div class="setting__icon">
+                        <svg
+                          id="Layer_1"
+                          data-name="Layer 1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 170.75 181.2"
+                          width="30px"
+                        >
+                          <defs>
+                            <style>
+                              .cls-1 {
+                                fill: #4e28eb;
+                                stroke: #fff;
+                                stroke-width: 7px;
+                              }
+                              .cls-1,
+                              .cls-2 {
+                                stroke-miterlimit: 10;
+                              }
+                              .cls-2,
+                              .cls-3 {
+                                fill: #fff;
+                              }
+                              .cls-2 {
+                                stroke: #231f20;
+                              }
+                            </style>
+                          </defs>
+                          <polygon
+                            class="cls-1"
+                            points="3.5 33.73 85.37 3.73 167.25 33.73 167.25 147.47 85.37 177.47 3.5 147.47 3.5 33.73"
+                          />
+                          <path
+                            class="cls-2"
+                            d="M100,68.13"
+                            transform="translate(-14.63 -9.4)"
+                          />
+                          <circle class="cls-3" cx="85.37" cy="62.6" r="9.18" />
+                          <circle class="cls-3" cx="85.37" cy="90.6" r="9.18" />
+                          <circle
+                            class="cls-3"
+                            cx="85.37"
+                            cy="118.6"
+                            r="9.18"
+                          />
+                        </svg>
+                      </div>
+                      <div class="edit__player-icon st__icon" data-id=${player.id}>
+                        <svg
+                          id="Layer_1"
+                          data-name="Layer 1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 106.05 105.56"
+                          width="13px"
+                        >
+                          <defs>
+                          
+                          </defs>
+                          <path
+                            class="cls-1"
+                            d="M99.86,68.07"
+                            transform="translate(-46.97 -47.22)"
+                          />
+                          <path
+                            d="M153,66.05a11.29,11.29,0,0,1-3.55,5.45c-2.87,2.78-5.67,5.65-8.51,8.48-.26.26-.54.52-.74.71L118.67,59.13c.12-.12.34-.37.57-.6l8.12-8.12c4.27-4.25,10-4.25,14.25,0,2.65,2.63,5.24,5.3,7.92,7.9a10.76,10.76,0,0,1,3.5,5.47Z"
+                            transform="translate(-46.97 -47.22)"
+                          />
+                          <path
+                            d="M79.79,140.83c-7.06-7-14.22-14.18-21.3-21.26L113,65.15l21.25,21.26Z"
+                            transform="translate(-46.97 -47.22)"
+                          />
+                          <path
+                            d="M52.36,125.6,73.77,147l-6.14,1.51-17,4.12c-2.7.65-4.12-.68-3.52-3.38Q49.72,137.42,52.36,125.6Z"
+                            transform="translate(-46.97 -47.22)"
+                          />
+                        </svg>
+                      </div>
+                      <div class="delete__player-icon st__icon" data-id=${player.id}>
+                        <svg
+                          id="Layer_1"
+                          data-name="Layer 1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 105.68 136.12"
+                          width="10px"
+                        >
+                          <path
+                            class="cls-1"
+                            d="M100,68.13"
+                            transform="translate(-47.16 -31.94)"
+                          />
+                          <circle
+                            class="cls-2"
+                            cx="52.84"
+                            cy="40.06"
+                            r="9.18"
+                          />
+                          <circle
+                            class="cls-2"
+                            cx="52.84"
+                            cy="68.06"
+                            r="9.18"
+                          />
+                          <circle
+                            class="cls-2"
+                            cx="52.84"
+                            cy="96.06"
+                            r="9.18"
+                          />
+                          <path
+                            d="M119.15,31.94c1.92,2,3.9,3.95,5.75,6a4.53,4.53,0,0,0,3.81,1.56c7.49-.08,15,0,22.46,0h1.67v15H47.16v-15H48.7c7.84,0,15.68,0,23.53,0a2.74,2.74,0,0,0,2.21-.89c1.87-2,3.81-3.83,5.72-5.75a9.27,9.27,0,0,0,.7-.92Z"
+                            transform="translate(-47.16 -31.94)"
+                          />
+                          <path
+                            d="M54.65,62.29h90.69c0,.51.06.94.06,1.38q0,44.25,0,88.5c0,7.81-4.55,13.79-11.78,15.48a17.51,17.51,0,0,1-3.82.39q-29.78,0-59.55,0c-9,0-15.61-6.44-15.62-15.43q-.09-44.66,0-89.31C54.61,63,54.63,62.7,54.65,62.29Z"
+                            transform="translate(-47.16 -31.94)"
+                          />
+                        </svg>
+                      </div>
                     </div>
                     <div class="player__rating">
                       <div class="player__rating-rate">${player.rating}</div>
@@ -338,3 +461,33 @@ const showPlayerCard = () => {
 
 getPlayerData();
 showPlayerCard();
+
+const settingIcon = document.querySelectorAll(".setting__icon");
+settingIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let parent = icon.parentElement;
+    parent.classList.toggle("active");
+    console.log(parent);
+  });
+});
+
+
+const deletePlayer = (e) => {
+  let target = e.target;
+  let getPlayerId = target.getAttribute("data-id");
+  console.log(getPlayerId);
+
+  const playerIndex = players.findIndex((player) => player.id === getPlayerId);
+
+  if (playerIndex !== -1) {
+    players.splice(playerIndex, 1); 
+    savePlayers(); 
+    showPlayerCard();
+    console.log(`Player with ID ${getPlayerId} deleted successfully.`);
+  } else {
+    console.warn(`Player with ID ${getPlayerId} not found.`);
+  }
+}
+
+const deleteBtn = document.querySelector(".delete__player-icon");
+deleteBtn.addEventListener("click", deletePlayer)
